1691f15166e380f346635627a2c4f8bba42cf3d9
emmi0122 <emiliaidajarleback@gmail.com>
2025-05-19 11:24:10 +0200

    Making some changes and adding Employee.vue

--- Uppgift.3/src/App.vue
+++ Uppgift.3/src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <Sidebar />
+  <div><Employee /></div>
 </template>
 
 <script setup>
 import Sidebar from './components/Sidebar.vue'
+import Employee from './components/Employee.vue';
+import EmployeeShifts from './components/EmployeeShifts.vue';
 </script>
 
 <style>
 body {
   margin: 0;
   font-family: 'Inter', sans-serif;
-}
-
-#app {
-  display: flex;
 }
 </style>
---
+++ Uppgift.3/src/components/Employee.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="employee-card" v-for="(em, index) in employeeNames" :key="index">
+        <img src="../assets/profile-picture.webp" alt="Profile-picture">
+        <div class="employee-info">
+            <h4>{{ em }}</h4>
+            <p>{{ employeeProfessions[index] }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const employeeNames = ["Albert Svensson", "Doris Johnson", "Moa Haag", "Stefan Holm", "Fatima Diame", "Harrison Ford", "Daniel García", "Joanna Rudzki", "David Lynch", "Anders Ofvegard"]
+const employeeProfessions = ["Elektriker, Murare, Snickare, VVS", "Snickare", "Elektriker", "Elektriker, VVS", "Murare", "Snickare", "Målare", "Murare, Målare", "Målare", "Arg snickare"]
+</script>
+
+<style scoped>
+img {
+    width: 3rem;
+}
+
+.employee-card {
+    width: 10rem;
+    border: solid black;
+    display: flex;
+    align-items: center;
+}
+
+.employee-info h4 {
+    margin: 0;
+    font-size: small;
+}
+
+.employee-info p {
+    margin: 0;
+    font-size: smaller;
+    display: flex;
+}
+</style>
